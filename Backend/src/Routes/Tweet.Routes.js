cd06726041958b8234b362f2fa816d53c2eef8ac
Rishu Raj <rishuraj1127@gmail.com>
2025-01-12 15:18:11 +0530

    jwt and multer added

--- Backend/src/Routes/Tweet.Routes.js
+++ Backend/src/Routes/Tweet.Routes.js
@@ -1,9 +1,24 @@
 import { Router } from "express";
-import {} from "../Controller/Tweet.controller.js"
-import router from "./User.Routes";
+import {
+  getTweetByUser,
+  getAllTweet,
+  deleteTweet,
+  UpdateTweet,
+  getTweetbyId,
+  createTweet,
+} from "../Controller/Tweet.controller.js";
+import { verifyJWT } from "../Middleware/Auth.middleware.js";
+import { upload } from "../Middleware/Multer.middleware.js";
 
 const router = Router();
 
-router.route("/").post()
+router.route("/getTweetByUser").post(verifyJWT, getTweetByUser);
+router.route("/getAllTweet").get(getAllTweet);
+router.route("/deleteTweet/:questionId").delete(verifyJWT, deleteTweet);
+router.route("/UpdateTweet/:questionId").patch(verifyJWT, UpdateTweet);
+router.route("/getTweetbyId/:questionId").get(verifyJWT, getTweetbyId);
+router
+  .route("/createTweet")
+  .post(verifyJWT, upload.single("media"), createTweet);
 
-export default router
+export default router;
