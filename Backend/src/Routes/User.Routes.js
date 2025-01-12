7c37f6eb6fd81195bc9258b3d511cde81a4df633
Rishu Raj <rishuraj1127@gmail.com>
2025-01-12 15:17:41 +0530

    verifyJWT and Multer middleware added

--- Backend/src/Routes/User.Routes.js
+++ Backend/src/Routes/User.Routes.js
@@ -9,15 +9,19 @@ import {
   loginUser,
   logoutUser,
 } from "../Controller/User.controller.js";
+import { verifyJWT } from "../Middleware/Auth.middleware.js";
+import { upload } from "../Middleware/Multer.middleware.js";
 const router = Router();
 
-router.route("/RegisterUser").post(RegisterUser);
+router.route("/RegisterUser").post(upload.single("avatar"), RegisterUser);
 router.route("/loginUser").post(loginUser);
-router.route("/logoutUser").post(logoutUser);
+router.route("/logoutUser").post(verifyJWT, logoutUser);
 router.route("/getCurrentUser").get(getCurrentUser);
 router.route("/getUserbyId/:userId").get(getUserbyId);
-router.route("/updateAccountDetails").put(updateAccountDetails);
-router.route("/updateUserAvatar").put(updateUserAvatar);
-router.route("/changeCurrentPassword").put(changeCurrentPassword);
+router.route("/updateAccountDetails").put(verifyJWT, updateAccountDetails);
+router
+  .route("/updateUserAvatar")
+  .put(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/changeCurrentPassword").put(verifyJWT ,changeCurrentPassword);
 
 export default router;
