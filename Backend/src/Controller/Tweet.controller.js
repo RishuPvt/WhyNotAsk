f47145d8506d08f743b4dcf745a1602d943b774a
Rishu Raj <rishuraj1127@gmail.com>
2025-01-12 15:18:59 +0530

    final tweet controller

--- Backend/src/Controller/Tweet.controller.js
+++ Backend/src/Controller/Tweet.controller.js
@@ -2,12 +2,11 @@ import { ApiError } from "../Utlis/ApiError.js";
 import { ApiResponse } from "../Utlis/ApiResponse.js";
 import { asyncHandler } from "../Utlis/Asynchandler.js";
 import { uploadOnCloudinary, deleteOnCloudinary } from "../Utlis/Clodinary.js";
-import Prisma from "../DB/DataBase.js";
 import prisma from "../DB/DataBase.js";
 
 const createTweet = asyncHandler(async (req, res) => {
   const { title, description, tags } = req.body;
-  const userId = req.user.id;
+  const userId = req.user?.id;
 
   if (!title || !description || !tags) {
     throw new ApiError(400, "All fields are required.");
@@ -62,7 +61,7 @@ const getAllTweet = asyncHandler(async (req, res) => {
 });
 
 const getTweetByUser = asyncHandler(async (req, res) => {
-  const userId = req.user.id;
+  const userId = req.user?.id;
 
   const user = await prisma.user.findUnique({
     where: {
@@ -92,14 +91,46 @@ const getTweetbyId = asyncHandler(async (req, res) => {
     },
   });
   if (!question) {
-    throw new ApiError(404, "question not found");
+    throw new ApiError(404, "tweet not found");
   }
   return res
     .status(200)
-    .json(new ApiResponse(200, question, "question fetched successfully"));
+    .json(new ApiResponse(200, question, "tweet fetched successfully"));
 });
 
-const UpdateTweet = asyncHandler(async (req, res) => {});
+const UpdateTweet = asyncHandler(async (req, res) => {
+  const { questionId } = req.params;
+
+  const question = await prisma.question.findUnique({
+    where: {
+      id: questionId,
+    },
+  });
+  if (!question) {
+    throw new ApiError(404, "tweet not found");
+  }
+
+  const updateFields = {};
+  if (title) updateFields.title = title;
+  if (description) updateFields.description = description;
+  if (tags) updateFields.tags = tags;
+
+  const updatedquestion = await prisma.question.update({
+    where: {
+      id: questionId,
+    },
+    data: updateFields,
+  });
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        updatedquestion,
+        "tweet details updated successfully"
+      )
+    );
+});
 
 const deleteTweet = asyncHandler(async (req, res) => {
   const { questionId } = req.params;
@@ -109,11 +140,18 @@ const deleteTweet = asyncHandler(async (req, res) => {
     },
   });
   if (!question) {
-    throw new ApiError(404, "question not found");
+    throw new ApiError(404, "tweet not found");
   }
   return res
     .status(200)
-    .json(new ApiResponse(200, question, "question Deleted successfully"));
+    .json(new ApiResponse(200, question, "tweet Deleted successfully"));
 });
 
-export { createTweet, getAllTweet, getTweetByUser, getTweetbyId ,deleteTweet };
+export {
+  createTweet,
+  getAllTweet,
+  getTweetByUser,
+  getTweetbyId,
+  deleteTweet,
+  UpdateTweet,
+};
