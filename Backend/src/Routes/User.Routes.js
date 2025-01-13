6346c0a0f38ec7ab9119755c48f46a304c56da34
Rishu Raj <rishuraj1127@gmail.com>
2025-01-14 00:07:51 +0530

    final user Route

--- Backend/src/Routes/User.Routes.js
+++ Backend/src/Routes/User.Routes.js
@@ -16,12 +16,12 @@ const router = Router();
 router.route("/RegisterUser").post(upload.single("avatar"), RegisterUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/getCurrentUser").get(verifyJWT ,getCurrentUser);
-router.route("/getUserbyId/:userId").get(verifyJWT ,getUserbyId);
+router.route("/getCurrentUser").get(verifyJWT, getCurrentUser);
+router.route("/getUserbyId/:userId").get(verifyJWT, getUserbyId);
 router.route("/updateAccountDetails").patch(verifyJWT, updateAccountDetails);
 router
   .route("/updateUserAvatar")
   .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
-router.route("/changeCurrentPassword").post(verifyJWT ,changeCurrentPassword);
+router.route("/changeCurrentPassword").post(verifyJWT, changeCurrentPassword);
 
 export default router;
