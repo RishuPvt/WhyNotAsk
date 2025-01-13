8b8b3a94fe16a4ef599bd5c45f1dc97287779fe9
Rishu Raj <rishuraj1127@gmail.com>
2025-01-14 00:07:14 +0530

    Cors added

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -1,6 +1,19 @@
 import express from "express"
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 const app=express()
+
+const corsOptions = { 
+   origin: ' http://localhost:5173',
+   credentials: true, 
+   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], 
+   allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'Authorization'], 
+ };
+ 
+ app.use(cors(corsOptions));
+
+
 
 // Middleware to parse JSON payloads with a size limit of 16kb
 app.use(express.json({ limit: "16kb" }));
