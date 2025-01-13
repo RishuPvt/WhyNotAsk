6c5ce98a884f509d70263fe77679b4d4ad848755
Rishu Raj <rishuraj1127@gmail.com>
2025-01-14 00:08:13 +0530

    final tweet Route

--- Backend/src/Routes/Tweet.Routes.js
+++ Backend/src/Routes/Tweet.Routes.js
@@ -6,13 +6,14 @@ import {
   UpdateTweet,
   getTweetbyId,
   createTweet,
+  getAllTweetByTags,
 } from "../Controller/Tweet.controller.js";
 import { verifyJWT } from "../Middleware/Auth.middleware.js";
 import { upload } from "../Middleware/Multer.middleware.js";
 
 const router = Router();
 
-router.route("/getTweetByUser").post(verifyJWT, getTweetByUser);
+router.route("/getTweetByUser").get(verifyJWT, getTweetByUser);
 router.route("/getAllTweet").get(getAllTweet);
 router.route("/deleteTweet/:questionId").delete(verifyJWT, deleteTweet);
 router.route("/UpdateTweet/:questionId").patch(verifyJWT, UpdateTweet);
@@ -20,5 +21,5 @@ router.route("/getTweetbyId/:questionId").get(verifyJWT, getTweetbyId);
 router
   .route("/createTweet")
   .post(verifyJWT, upload.single("media"), createTweet);
-
+router.route("/getAllTweetByTags/:tag").get(verifyJWT, getAllTweetByTags);
 export default router;
